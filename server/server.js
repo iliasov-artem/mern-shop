e1009e9357c6d34c477478478622888e7fa9d661
artem-ilyasov <fmlp@yandex.ru>
2020-10-17 13:21:25 +0300

    add error handlers

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config();
@@ -15,7 +15,12 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
